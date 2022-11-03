9ad28ac89c9506c3be3c6e78516b205d2957065b
Davi Moisés <moisesdavi005@gmail.com>
2022-11-03 10:20:42 -0300

    Fix: corrigindo o modal de login e cadastro

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -5,18 +5,13 @@ import { Toast } from "@chakra-ui/react";
 import { api } from "../services/api";
 
 /* import { createContext } from "react"; */
-import React, {  Dispatch } from 'react'
-interface IModalLogin {
-  modal: boolean,
-  setModal: Dispatch<boolean>
-}
+import React, { Dispatch } from 'react'
 interface iUserContext {
   submitRegister: (body: iRegisterBody) => Promise<void>;
   submitLogin: (body: iRegisterBody) => Promise<void>;
   modalControl: boolean;
   modalType: "login" | "register";
   setModalControl: React.Dispatch<React.SetStateAction<boolean>>;
-  /*  setModalType: React.Dispatch<React.SetStateAction<iModalType>>; */
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
@@ -43,16 +38,13 @@ export const UserContext = createContext<iUserContext>({} as iUserContext);
 
 export const UserProvider = ({ children }: iUserContextProps) => {
   const [modalControl, setModalControl] = useState<boolean>(false);
-
-  /* const [modalType, setModalType] = useState<iModalType>({ type: "login" }); */
   const { isOpen, onOpen, onClose } = useDisclosure();
-
   const [modalType, setModalType] = useState<"login" | "register">("login");
 
   const submitRegister = async (body: iRegisterBody): Promise<void> => {
     try {
       console.log(body);
-      //   const { data } = await api.post("register", body);
+         const { data } = await api.post("register", body);
       Toast({
         title: "Cadastro efetuado com sucesso!",
         description: "Você já pode logar com sua nova conta.",
@@ -61,7 +53,7 @@ export const UserProvider = ({ children }: iUserContextProps) => {
         isClosable: true,
         position: "top",
       });
-      /* setModalType({ type: "register" }); */
+      setModalType("register");
     } catch (error) {
       console.log(error);
     }
@@ -70,7 +62,7 @@ export const UserProvider = ({ children }: iUserContextProps) => {
   const submitLogin = async (body: iLoginBody): Promise<void> => {
     try {
       console.log(body);
-      //   const { data } = await api.post("login", body);
+        const { data } = await api.post("login", body);
       Toast({
         title: "Login efetuado com sucesso!",
         duration: 2000,
@@ -78,9 +70,7 @@ export const UserProvider = ({ children }: iUserContextProps) => {
         isClosable: true,
         position: "top",
       });
-      /* setModalType({ type: "login" }); */
       setModalType("login");
-
     } catch (error) {
       console.log(error);
     }
@@ -103,19 +93,6 @@ export const UserProvider = ({ children }: iUserContextProps) => {
     </UserContext.Provider>
   );
 }
-
-
-/* export const UserProvider = ({ children }: any) => {
-
-  const [modal, setModal] = useState<IModalLogin>(false)
-  const closeModal = () => {
-      setModal(false)
-    }
-
-  return <UserContext.Provider value={{modal, setModal}}>{children}</UserContext.Provider>;
-
-}; */
-
 
 export const useContextFunction = () => {
   const contextUser = useContext(UserContext)
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,93 +1,36 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Modal, ModalContent, ModalOverlay, } from "@chakra-ui/react";
+import { useContext } from "react";
 import { HeaderLandingPage } from "../components/HeaderLandingPage";
 import { MainLandingPage } from "../components/MainLandingPage";
-/* import { Modal } from "../components/Modal"; */
-import { Box, /* Modal ,*/ ModalContent, ModalOverlay } from "@chakra-ui/react";
-import { useContext } from "react";
-/* import { HeaderLandingPage } from "../components/HeaderLandingPage"; */
-/* import { MainLandingPage } from "../components/MainLandingPage"; */
+import { ModalLogin } from "../components/ModalLogin";
 import { RegistForm } from "../components/RegistForm";
 import { UserContext } from "../contexts/userContext";
 
+
 export const LandingPage = () => {
-  return (
-  <Flex direction={"column"} w="100vw" h="100vh">
-    <HeaderLandingPage />
-    <MainLandingPage />
-  </Flex>
-  )
-}
-
-
-/* export const LandingPage = () => {
-  const { modalControl, modalType } = useContext(UserContext);
+  const { modalControl, modalType, isOpen, onClose } = useContext(UserContext);
   return (
     <>
       <Box w="100vw" h="100vh">
         <HeaderLandingPage />
         <MainLandingPage />
         <Modal
+          /* isOpen={isOpen}  */
+         /*  onClose={onClose} */
           isCentered
           isOpen={modalControl}
           closeOnOverlayClick={true}
-          onClose={() => {}}
+          onClose={() => { }}
         >
           <ModalOverlay />
           <ModalContent w="800px">
-            {modalType === "login" ? <h1>Login</h1> : <RegistForm />}
+            {modalType === "login" ? <ModalLogin /> : <RegistForm />}
           </ModalContent>
         </Modal>
       </Box>
     </>
+  )
+}
 
-import { Box, Button, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
-import { LoginPage } from "./LoginPage";
 
-export const LandingPage = () => {
-  return (
-    <Flex direction={"column"} w="100vw" h="100vh">
-    
-      <Flex
-        as={"header"}
-        w="100%"
-        align="center"
-        justify="space-between"
-        p="5px 40px"
-        m="0 auto"
-        minH="100px"
-        bg="#2C7AED"
-        sx={{ boxShadow: "0px 4px 9px rgba(0, 0, 0, 0.507)" }}
-      >
-        <Flex align="center" gap="20px">
-          <Image src="./src/assets/img/logo.svg" />
-          <Heading color="#DAEEFA"> Bookworm Library</Heading>
-        </Flex>
-        <Stack align="center" direction={"row"} color="white" gap="20px">
-          <Button
-            sx={{ boxShadow: "0px 4px 9px rgba(0, 0, 0, 0.315)" }}
-            _hover={{ opacity: "0.7" }}
-            bgGradient="linear(to-t,#2CEDE0, #2C7AED)"
-            onClick={LoginPage}
-          >
-            Login
-          </Button>
-          <Button
-            sx={{ boxShadow: "0px 4px 9px rgba(0, 0, 0, 0.315)" }}
-            _hover={{ opacity: "0.7" }}
-            bgGradient="linear(to-t,#2CEDE0, #2C7AED)"
-          >
-            Cadastro
-          </Button>
-        </Stack>
-      </Flex>
-      <Flex as={"main"} bgImage="./src/assets/img/bgbooks.svg" h={"100%"}  p={"20px"}  align={"center"} justify="center">
-        <Image src="./src/assets/img/books.png"/>
-        <Flex bg={"#2C7AED"} h="85%" align={"center"} justify="center" p={"5px"}>
-          <Text color={"white"}>Descrição sobre o que o usuario irá encontrar no site...........</Text>
-        </Flex>
-      </Flex>
-    </Flex>
->>>>>>> 0f9c77a1124bf56599b36c8ba09ecaf3c204bd13
-  );
-}; */
 
