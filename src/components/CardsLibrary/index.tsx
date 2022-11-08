b239229a130a3bc314551222e9ec8fb8cf0766f4
Gabriel_Machado <gbrasil200@gmail.com>
2022-11-08 10:28:10 -0300

    fix: library modal function

--- src/components/CardsLibrary/index.tsx
+++ src/components/CardsLibrary/index.tsx
@@ -1,11 +1,10 @@
-import {
-  UnorderedList,
-} from "@chakra-ui/react";
+import { UnorderedList } from "@chakra-ui/react";
 import { useContext } from "react";
 import { SearchContext } from "../../contexts/searchContext/searchContext";
 import { iBooks } from "../../contexts/searchContext/searchContext";
 import { ListCard } from "../ListCard";
 import { useContextFunction } from "../../contexts/userContext/userContext";
+import { ModalLibrary } from "../ModalLibrary";
 
 const CardsLibrary = () => {
   const { library, setCurrentBook, loading } = useContext(SearchContext);
@@ -36,101 +35,21 @@ const CardsLibrary = () => {
       >
         {library?.map((element: iBooks) => {
           return (
-            <ListCard key={element.id} element={element} buttonTitle={"Ver Livro"} />
-          )
-        })}
-      </UnorderedList>
-    </>
-  );
-};
-  /* return library?.length === 0 ? (
-    <h2> Você não possui livros na sua biblioteca! </h2>
-  ) : (
-    <>
-      <UnorderedList
-        display={"flex"}
-        gap="30px"
-        w="100%"
-        flexWrap={"wrap"}
-        h="900px"
-        overflowY="auto"
-        css={{
-          "&::-webkit-scrollbar": {
-            width: "4px",
-          },
-          "&::-webkit-scrollbar-track": {
-            width: "6px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            borderRadius: "24px",
-          },
-        }}
-      >
-        {library?.map((element: any) => {
-          return (
-            <ListItem
+            <ListCard
               key={element.id}
-              w="30%"
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              gap="1.5rem"
-              margin={0}
-              padding={"1rem"}
-              marginBottom="1.5rem"
-              bgGradient="linear(to-t, cyan, blue.light)"
-              borderRadius="4px"
-            >
-              <Flex align={"start"} p="10px">
-                <Image
-                  src={
-                    element.volumeInfo?.imageLinks
-                      ? element.volumeInfo.imageLinks?.thumbnail
-                      : unknownImage
-                  }
-                  w="8.25rem"
-                  h="12.5rem"
-                  margin={0}
-                  padding={0}
-                />
-                <Box p="0px 15px" color={"white"} overflow="hidden">
-                  <Heading as="h3" fontSize={"20px"}>
-                    Título:
-                  </Heading>
-                  <Text as="span" fontSize={"16px"} noOfLines={[1, 2, 3]}>
-                    {element.volumeInfo?.title}
-                  </Text>
-                  <Heading as="h3" fontSize={"20px"} marginTop="10px">
-                    Autor:
-                  </Heading>
-                  <Text as="span" fontSize={"16px"} noOfLines={[1]}>
-                    {element.volumeInfo?.authors}
-                  </Text>
-                </Box>
-              </Flex>
-              <Stack p="10px 0px">
-                <Button
-                  color="white"
-                  bg="#2C7AED"
-                  _hover={{ opacity: "0.7" }}
-                  w="13.75rem"
-                  h="2.625rem"
-                  onClick={(e) => {
-                    e.preventDefault;
-                    onOpen();
-                    setCurrentBook(element);
-                  }}
-                >
-                  Ver Livro
-                </Button>
-                <ModalLibrary isOpen={isOpen} onClose={onClose} />
-              </Stack>
-            </ListItem>
+              element={element}
+              buttonTitle={"Ver Livro"}
+              itemAction={() => {
+                setCurrentBook(element);
+                onOpen();
+              }}
+            />
           );
         })}
       </UnorderedList>
-    </> 
+      <ModalLibrary isOpen={isOpen} onClose={onClose} />
+    </>
   );
-};*/
+};
 
 export default CardsLibrary;
